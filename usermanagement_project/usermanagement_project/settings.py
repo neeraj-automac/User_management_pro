d9d6c8d2751020e2680c6a6ec38171226b557af1
neeraj <neeraj.automac@gmail.com>
2024-10-23 12:04:27 +0530

    hct_static root update

--- usermanagement_project/usermanagement_project/settings.py
+++ usermanagement_project/usermanagement_project/settings.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
+import os
 from datetime import timedelta
 from pathlib import Path
 
@@ -171,7 +172,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
+# STATIC_URL = 'static/'
 STATIC_URL = 'static/'
+STATIC_ROOT = '/var/www/hct_static/static/'
+# STATICFILES_DIRS=[
+#     os.path.join(BASE_DIR,'build/static')
+# ]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field

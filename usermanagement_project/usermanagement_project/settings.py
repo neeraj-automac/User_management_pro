7c80ae7427c5bf9ccf861c8e83b6f99d3d427651
neeraj <neeraj.automac@gmail.com>
2024-10-23 14:19:02 +0530

    jwt auth enabled in settings

--- usermanagement_project/usermanagement_project/settings.py
+++ usermanagement_project/usermanagement_project/settings.py
@@ -93,12 +93,12 @@ TEMPLATES = [
 WSGI_APPLICATION = 'usermanagement_project.wsgi.application'
 
 REST_FRAMEWORK = {
-    # 'DEFAULT_PERMISSION_CLASSES': (
-    #     'rest_framework.permissions.IsAuthenticated',
-    # ),
-    # 'DEFAULT_AUTHENTICATION_CLASSES': (
-    #     'rest_framework_simplejwt.authentication.JWTAuthentication',
-    # ),
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
+    ),
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
 }
 
 SIMPLE_JWT = {

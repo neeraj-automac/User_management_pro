958dcfc1fc8064472401212dda1090fdc1c72ece
neeraj <neeraj.automac@gmail.com>
2025-05-06 15:39:51 +0530

    accesstoken to 3min

--- usermanagement_project/usermanagement_project/settings.py
+++ usermanagement_project/usermanagement_project/settings.py
@@ -109,7 +109,8 @@ REST_FRAMEWORK = {
 }
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(days=1),
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=5),
+    # 'ACCESS_TOKEN_LIFETIME': timedelta(days=1),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=2),
     'ROTATE_REFRESH_TOKENS': True,
     'BLACKLIST_AFTER_ROTATION': True,
